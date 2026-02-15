d25b60e9dacf710c37f5b4b496c4c86632cd4dd9
Iris Xie <43115060+eilixirs@users.noreply.github.com>
2026-02-14 23:27:14 -0500

    edited about me content

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -11,23 +11,25 @@ export function About() {
           <div>
             <DecoratedHeading.Root level={1}>
               <DecoratedHeading.Background>嗨～我是思睿</DecoratedHeading.Background>
-              <DecoratedHeading.Content>Hi! I&apos;m Iris</DecoratedHeading.Content>
+              <DecoratedHeading.Content>Hey! I&apos;m Iris</DecoratedHeading.Content>
             </DecoratedHeading.Root>
             <Text className="mb-5">
-              I&apos;m a strategic marketer and creative problem-solver with a
-              passion for building brands that resonate. Currently pursuing a
-              Bachelor of Commerce at Queen&apos;s University, Canada.
+              I&apos;m a strategic marketer and creative problem-solver who&apos;s been obsessed
+              with design since day one. I&apos;sve got passion for building brands that resonate
+              and systems that feel frictionless. Currently pursuing a Bachelor of Commerce at
+              Queen&apos;s University, Canada.
             </Text>
             <Text className="mb-5">
-              Beyond marketing, I love when technology and design collide.
-              I&apos;m fond of photography, UX/UI design, and efficiency.
+              I&apos;m fond of photography, UX/UI design, and context engineering.
               I&apos;ve traveled to 26 countries, and can hold a conversation
               with anyone in Mandarin Chinese.
             </Text>
             <Text>
               I thrive on turning vague ideas into compelling visuals, whether
               through slide-driven presentations, brand guidelines, or content
-              that drives engagement.
+              that drives engagement. Above all, I&apos;sm a sucker for telling 
+              story of innovative, impactful projects, and I&apos;sm hooked on
+              any way to improve my efficiency.
             </Text>
           </div>
 
