72d3dc38227ff8c079ae5a81558387b4cc8cb5e9
eilixirs <irisxie@outlook.com>
2026-06-22 04:32:32 -0400

    fix: spacing adjustment

--- src/components/projects/ProjectGrid.tsx
+++ src/components/projects/ProjectGrid.tsx
@@ -3,7 +3,7 @@ import { projects } from "@/data/projects";
 
 export function ProjectGrid() {
   return (
-    <div className="grid sm:grid-cols-3 grid-cols-2 auto-rows-fr gap-12">
+    <div className="grid sm:grid-cols-3 grid-cols-2 auto-rows-fr gap-8">
       {projects.map((project) => (
         <ProjectCard key={project.name} project={project} />
       ))}
